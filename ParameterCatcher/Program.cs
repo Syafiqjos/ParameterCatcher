d6814684f400ee7df24f8601fb984e122a18201b
Syafiqjos <syafiqjos@gmail.com>
2020-11-06 14:44:41 +0700

    commit test on old project

---
+++ ParameterCatcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ParameterCatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Parameter;
+            Parameter = string.Join(" ", args);
+            string[] arr = {Parameter};
+
+            Console.WriteLine(Parameter);
+            File.WriteAllLines(@"D:\]DovahkiiND[\ParameterCatcher\ParameterCatcher\ParameterCatcher\bin\Release\output.txt", arr);
+            Console.ReadLine();
+        }
+    }
+}
